80aec590c4b8512323a3011394c64900cd44bc46
trongltgit <tronglt.hcm@gmail.com>
2025-09-20 15:58:38 +0700

    Update pdf_reader.py

--- utils/pdf_reader.py
+++ utils/pdf_reader.py
@@ -2,8 +2,10 @@ from pypdf import PdfReader
 
 def extract_chunks(file_path, chunk_size=500):
     reader = PdfReader(file_path)
-    text = ""
+    chunks = []
     for page in reader.pages:
-        text += page.extract_text() or ""
-    chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+        text = page.extract_text()
+        if text:
+            for i in range(0, len(text), chunk_size):
+                chunks.append(text[i:i+chunk_size])
     return chunks
